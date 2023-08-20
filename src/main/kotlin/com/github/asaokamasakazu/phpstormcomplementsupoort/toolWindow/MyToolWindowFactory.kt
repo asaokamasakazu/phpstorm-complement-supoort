834a72534dad2a2c7806cd10e7d1cfd34960f647
GitHub Action <action@github.com>
2023-08-20 03:22:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/asaokamasakazu/phpstormcomplementsupoort/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/asaokamasakazu/phpstormcomplementsupoort/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.asaokamasakazu.phpstormcomplementsupoort.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.asaokamasakazu.phpstormcomplementsupoort.MyBundle
+import com.github.asaokamasakazu.phpstormcomplementsupoort.services.MyProjectService
 import javax.swing.JButton
 
 
